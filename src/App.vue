07af9489383c37e24b0ae92d56bb8f4ce2a3d713
vbertazzo <vbertazzo@gmail.com>
2021-05-11 14:38:54 -0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1>Hello, world.</h1>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'App'
+})
+</script>
+
+<style></style>
