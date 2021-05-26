287e273229ef47d05160a674cfa907d472dc4d04
vbertazzo <vbertazzo@gmail.com>
2021-05-25 23:27:00 -0300

    Configure tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,15 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   mode: 'jit',
   purge: ['./public/**/*.html', './src/**/*.{js,ts,vue}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {}
+    extend: {
+      fontFamily: {
+        sans: ['Montserrat', ...defaultTheme.fontFamily.sans]
+      }
+    }
   },
   variants: {
     extend: {}
